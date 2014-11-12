2dd9a13261289b7b4ef09c99e7b4e7e0913353a0
ma34s <ma34s.sss@gmail.com>
2014-11-12 11:12:44 +0900

    fix kernel  defconfig
    Change-Id: Iba881b83e248d9c487e90562bd1fb73c18663cf2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@
 
 TARGET_OTA_ASSERT_DEVICE := SC01F,hltedcm
 
+# Kernel
+TARGET_KERNEL_VARIANT_CONFIG := kbc_sc01f_aosp_defconfig
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_LIBINIT_DEFINES_FILE := device/samsung/hltedcm/init/init_hlte.c
